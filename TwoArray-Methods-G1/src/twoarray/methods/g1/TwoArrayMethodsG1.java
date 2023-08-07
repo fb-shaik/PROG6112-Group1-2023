05344186f9cfbce6238a966d86765e41a126c04c
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2023-08-07 09:51:14 +0200

    Add files via upload

---
+++ TwoArray-Methods-G1/src/twoarray/methods/g1/TwoArrayMethodsG1.java
@@ -0,0 +1,30 @@
+
+package twoarray.methods.g1;
+//Simple app to illustrate the use of 2D arrays & methods 
+public class TwoArrayMethodsG1 {
+
+    public static void main(String[] args) {
+        //declare a 2D array 
+        //datatype arrayName [R][C] = {{},{},{}}
+        int arr_2d[][] = {{1,2,3},//R1
+                                          {4,5,6},//R2
+                                          {7,8,9} };//R3
+        //array size [r][c]: Rows:3 & Columns: 3>>>>size is 9 (3x3)
+        
+    //method call to pass the 2D Array 
+    passArray(arr_2d);
+       
+    }
+    //create a method that will receive the above 2D array as a parameter
+    public static void passArray(int [][]arr_2d)//need to have [R][C] in the parameter list to show a 2d array is passed
+    {
+            //print values in the 2dArray require a nested loop to acces both the Rows & Columns of the 2D Array
+            for (int i=0 ; i <arr_2d.length; i++) //outer loop will deal with the Rows
+            {
+                for(int j = 0; j <arr_2d.length; j++)//inner loop will deal with the Columns
+                    {System.out.println(arr_2d[i][j] + " ");}
+            }
+                System.out.println();
+    }
+    
+}
